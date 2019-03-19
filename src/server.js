6f8c0d928558fdfa94a02334df89d524c007ce56
ErikMartensson <erik@acid.nu>
2019-03-19 23:46:58 +0100

    Gah!

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
-const port = process.ENV.PORT || 3000
+const port = process.env.PORT || 3000
 
 const buildPath = path.join(__dirname, '../build')
 const indexPath = path.join(buildPath, 'index.html')
