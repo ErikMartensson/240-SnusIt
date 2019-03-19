cd8191f5b1670e715a3e3b8dced797919c559f76
ErikMartensson <erik@acid.nu>
2019-03-19 23:33:22 +0100

    Forgot to use static build files in production

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+
+const buildPath = path.join(__dirname, '../build')
+const indexPath = path.join(buildPath, 'index.html')
+
+const index = fs.readFileSync(indexPath)
+const indexStat = fs.statSync(indexPath)
+
+app.use(express.static(buildPath, { maxage: 0 }))
+
+app.get('*', (req, res) => {
+	res.end(index)
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
