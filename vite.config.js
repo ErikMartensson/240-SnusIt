1115d45f92e2c0cdfe43760c0b6dc1ba7ca8c458
ErikMartensson <4602153+ErikMartensson@users.noreply.github.com>
2026-03-23 11:47:51 +0100

    Switched to Vite

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' },
+    },
+  },
+})
