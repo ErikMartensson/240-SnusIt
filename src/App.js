ce90a127ecf6560d1ebbcb2acf65e069a3ce19e7
ErikMartensson <4602153+ErikMartensson@users.noreply.github.com>
2026-03-23 12:17:55 +0100

    Improved Prettier integration

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Main from './Main'
 import PlayButton from './PlayButton'
 
 const GlobalStyle = createGlobalStyle`
-	html {
-		height: 100vh;
-	}
-	body {
-		font-family: "Source Sans Pro";
-		font-weight: 300;
-		background:
-			linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, 0)),
-			url(/static/img/bg${(props) => props.$img}.jpg) no-repeat center;
-		background-size: cover;
-		background-attachment: fixed;
-	}
-	a {
-		color: #68baff;
-		text-decoration: none;
-	}
-	a:hover {
-		text-decoration: underline;
-	}
+  html {
+    height: 100vh;
+  }
+  body {
+    font-family: "Source Sans Pro";
+    font-weight: 300;
+    background:
+      linear-gradient(rgba(0, 0, 0, .4), rgba(0, 0, 0, 0)),
+      url(/static/img/bg${(props) => props.$img}.jpg) no-repeat center;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+  a {
+    color: #68baff;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
 `
 
 const bgImages = [6, 7, 8, 9, 10, 11, 12, 13]
